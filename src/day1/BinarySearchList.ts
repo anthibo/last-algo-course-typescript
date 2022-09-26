945340a31be77b50c21174b394e67732532aef2b
Abdulrahman Khalid <anthiboSama@gmail.com>
2022-09-26 10:28:49 -0700

    implement binary search algorithm

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,14 @@
+// on assumption that array is sorted
+// time complexity of O(Log(N))
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let low = 0;
+    let high = haystack.length
+    while (low < high) {
+        const mid = Math.floor(low + (high - low) / 2)
+        const currentValue = haystack[mid]
+        if (currentValue === needle) { return true }
+        else if (currentValue > needle) { high = mid }
+        else { low = mid + 1 }
+    }
+    return false
 }
