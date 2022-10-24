51dfe52dce6631c5cbf5a7c497c06622bdf23bd5
Abdulrahman Khalid <anthiboSama@gmail.com>
2022-10-24 02:48:03 -0700

    wip: implementing LRU

--- src/day1/LRU.ts
+++ src/day1/LRU.ts
@@ -1,15 +1,44 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>,
+    prev?: Node<T>,
+}
+
+function createNode<V>(value: V): Node<V> {
+    return { value }
+}
+
 export default class LRU<K, V> {
     private length: number;
+    private head?: Node<V>;
+    private tail?: Node<V>;
 
-    
+    private lookup: Map<K, Node<V>>;
+    private reverseLookup: Map<Node<V>, K>;
 
-    constructor() {
+
+
+    constructor(private capacity: number = 10) {
+        this.length = 0;
+        this.head = this.tail = undefined;
+        this.lookup = new Map<K, Node<V>>()
     }
 
     update(key: K, value: V): void {
+        // does it exist ?
+        // get()
 
-}
+        //  if it does not ? -> insert
+        //      - check capacity and evict if over
+        //  if it does -> update it to the front of the list
+        //  and update the value
+
+    }
     get(key: K): V | undefined {
+        // check the cache for existence
+        const node = this.lookup.get(key)
+        // update the value we found and move it to the front
 
-}
+        // return out the value found or undefined if not found
+    }
 }
